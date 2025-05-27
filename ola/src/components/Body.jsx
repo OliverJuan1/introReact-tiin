61fb8a3e1331ace3108c90555764d866a79864cc
OliverJuan1 <Juan883462@gmail.com>
2025-05-26 22:18:51 -0300

    Introdução ao react

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos que não tinham pais 🥲</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum, reiciendis distinctio vel provident autem incidunt fugit labore voluptatum, rerum commodi in quae consequatur perferendis quasi dolorum quidem, numquam aliquam!</p>
+    </div>
+  )
+}
+
+export default Body
