872d5f90dce001430ef95de4dac350bb33e95f91
OliverJuan1 <Juan883462@gmail.com>
2025-05-28 20:50:35 -0300

    feat:primeiro form react

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,71 +1,17 @@
 import { useState } from 'react'
 import './Body.css'
 
+
 function Body() {
-  const [cont, setCont] = useState(0)
-  const [inputValor, setInputValor] = useState('')
-  const [inputN1, setInputN1] = useState('')
-  const [inputN2, setInputN2] = useState('')
-
-
-  function aumentar() {
-    setCont(cont + 1)
-  }
-
-  function diminuir() {
-    if (cont > 0) {
-      setCont(cont - 1)
-    }
-  }
-
-  function alterar() {
-    setCont(Number(inputValor))
-  }
-
-   function somar(){
-    setCont(Number(inputN1) + Number(inputN2))
-   }
-
-   function subs(){
-    setCont(Number(inputN1) - Number(inputN2))
-   }
-
-   function mult(){
-    setCont(Number(inputN1) * Number(inputN2))
-   }
-
   return (
-    <div className='Body'>
-      <h2 className='titulo2'>Dinos que não tinham pais 🥲</h2>
-      <div className='cont'>
-        <button onClick={diminuir}>-</button>
-        <p> {cont}</p>
-        <button onClick={aumentar}>+</button>
+    <div className='Body'>  
+      <div className='card'>
+        <div className='noImage'>Imagem indisponivel</div>
+        <h2>Personagem</h2>
+        <p>
+          <strong>Casa:</strong> Desconhecida
+        </p>
       </div>
-
-      <div className='form'>
-        <input type="number"
-          value={inputValor}
-          onChange={(e) => setInputValor(e.target.value)}
-        />
-        <button onClick={alterar}>Alterar</button>
-      </div>
-
-      <div className='calculadera'>
-        <input type="text" 
-        value={inputN1}
-        onChange={(e) => setInputN1(e.target.value)}
-        />
-       
-        <button onClick={somar}>+</button>
-        <button onClick={subs}>-</button>
-        <button onClick={mult}>*</button>
-        <input type="text" 
-        value={inputN2}
-        onChange={(e) => setInputN2(e.target.value)}
-        /> 
-      </div>
-
     </div>
   )
 }
--- ola/src/components/Header.jsx
+++ ola/src/components/Header.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
+  const [searchTerm , setSearchchTerm] = React.useState('')
   return (
-    <div className='Header'>
-        <h1>Dinossauros🦕</h1>
+    <div className='Header'> 
+        <h1>Buscador de Personagens - Harry Poter</h1>
+
+        <input type="text" 
+        placeholder='Digite o nome...'
+        value={searchTerm}
+        onChange={(e) => setSearchchTerm(e.target.value)}
+        className='search'
+        />
     </div>
   )
 }
